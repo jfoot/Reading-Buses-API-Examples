4391a9e78e75e37ebcd6a1e247aa8389c803ab0a
Jonathan Foot <jonathanfoot@hotmail.co.uk>
2020-06-08 17:56:23 +0100

    Updated to ReadingBusesAPI V2.0.0

--- ReadingBusesNewAPIWithLibrary/Program.cs
+++ ReadingBusesNewAPIWithLibrary/Program.cs
@@ -1,62 +1,72 @@
 ﻿using ReadingBusesAPI;
 using System;
-using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Threading.Tasks;
+using ReadingBusesAPI.Bus_Service;
+using ReadingBusesAPI.Bus_Stops;
+using ReadingBusesAPI.Journey_Details;
+using ReadingBusesAPI.Shared;
 
 namespace ReadingBusesNewAPIWithLibrary
 {
     class Program
     {
-        static async System.Threading.Tasks.Task Main(string[] args) {
+        static async Task Main(string[] args) {
             // Optional - Default is true, Caches Bus Stops and Bus Services Data (by default for 7 days) as you can assume this to not change regularly.
             ReadingBuses.SetCache(true);
             //Initializes the controller, enter in your Reading Buses API - Get your own from http://rtl2.ods-live.co.uk/cms/apiservice
             //Once Instantiated you can also use, "ReadingBuses.GetInstance();" to get future instances.
-            ReadingBuses Controller =  await ReadingBuses.Initialise("");
-            
-            string serviceOption;   //Used to store the user choice for which service to view
+            ReadingBuses controller =  await ReadingBuses.Initialise("");
+
+	        const Operators operators = Operators.ReadingBuses; //Used to store which operator to get data from.
+			string serviceOption;   //Used to store the user choice for which service to view
             string stopOption;      //Used to store the user choice for which bus stop to view.
 
             //Prints off all services running in Reading.
-            Controller.PrintServices();
+            foreach (var busService in controller.GetServices().Where(x => x.OperatorCode == operators))
+	            Console.WriteLine(busService.ServiceId + " " + busService.BrandName);
+			
+	            
 
-            do
+			do
             {
                 Console.WriteLine("\nWhich bus service do you want to review? Please enter it's service number.");
                 serviceOption = Console.ReadLine();
-            } while (!Controller.IsService(serviceOption)); //Checks the value entered is a ReadingBuses Service
+            } while (!controller.IsService(serviceOption, operators)); //Checks the value entered is a ReadingBuses Service
             Console.Clear();
 
             //Gets that service and stores it in a BusServices Object.
-            BusService service = Controller.GetService(serviceOption);
+            BusService service = controller.GetService(serviceOption, operators);
 
-            //Prints off the name and code of every bus stop visited by that service.
-            foreach (var location in service.GetLocations())
+            ////Prints off the name and code of every bus stop visited by that service.
+            foreach (var location in await service.GetLocations())
                 Console.WriteLine(location.CommonName + " : " + location.ActoCode);
 
-            do
-            {
-                Console.WriteLine("\nWhich stop do you wish to review? Please enter it's ID value.");
-                stopOption = Console.ReadLine();
-            } while (!Controller.IsLocation(stopOption)); //Checks the value entered is a Acto-Code for a bus stop.
-            Console.Clear();
+			do
+			{
+				Console.WriteLine("\nWhich stop do you wish to review? Please enter it's ID value.");
+				stopOption = Console.ReadLine();
+			} while (!controller.IsLocation(stopOption)); //Checks the value entered is a Acto-Code for a bus stop.
+			Console.Clear();
 
-            //Gets that bus stop and stores it in a BusStop Object.
-            BusStop stop = Controller.GetLocation(stopOption);
+			//Gets that bus stop and stores it in a BusStop Object.
+			BusStop stop = controller.GetLocation(stopOption);
 
-            //Loops continuously.
-            while (true)
-            {
-                //Gets Live Data from the bus stop selected.
-                List<LiveRecord> Arrivals = stop.GetLiveData();
+			//Loops continuously.
+			while (true)
+			{
+				//Gets Live Data from the bus stop selected.
+				LiveRecord[] arrivals = await stop.GetLiveData();
 
-                //Prints off the data for each service due to arrive at the stop.
-                Console.WriteLine(DateTime.Now.ToString());
-                foreach (LiveRecord bus in Arrivals)
-                    Console.WriteLine(bus.ServiceNumber +" - " + bus.Service().BrandName + "   " + bus.Destination + "		" + bus.SchArrival + "		" + bus.ExptArrival);
+				//Prints off the data for each service due to arrive at the stop.
+				Console.WriteLine(DateTime.Now.ToString(CultureInfo.InvariantCulture));
+				foreach (LiveRecord bus in arrivals)
+					Console.WriteLine(bus.ServiceNumber + " - " + bus.Service().BrandName + "   " + bus.Destination + "		" + bus.SchArrival + "		" + bus.ExptArrival);
 
-                Console.WriteLine("");
-                System.Threading.Thread.Sleep(30000);
-            }
-        }
+				Console.WriteLine("");
+				System.Threading.Thread.Sleep(30000);
+			}
+		}
     }
 }
